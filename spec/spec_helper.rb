a8331babf9299803cc829ae5134868a4b2a6ef6d
Shawn Toffel <shawn.toffel+github@gmail.com>
2014-02-02 21:59:09 -0700

    Update spec_helper.rb

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,10 +1,10 @@
+require 'coveralls'
+# coveralls test coverage
+Coveralls.wear!
+
 require 'ann_wrapper'
 require 'webmock/rspec'
 require 'support/fake_ann'
-require 'coveralls'
-
-# coveralls test coverage
-Coveralls.wear!
 
 WebMock.disable_net_connect!(allow_localhost: true)
 
